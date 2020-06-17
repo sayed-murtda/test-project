c51995b053a94127f2261d031ecb6ce3404dc0e8
sayed-murtda <67059474+sayed-murtda@users.noreply.github.com>
2020-06-17 18:30:25 +0300

    first

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author DNA
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /* f m = new f();
+        m.setVisible(true);
+        m.setLocationRelativeTo(null);*/
+        new f().setVisible(true);
+        
+    }
+    
+}
